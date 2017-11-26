500ef1d45803d4e87404eab8d70eaf33b7874954
yedaodao <404069912@qq.com>
2017-11-26 09:13:32 +0800

    test bug

--- support/httpCallback.js
+++ support/httpCallback.js
@@ -9,12 +9,13 @@ const maxCount = 1000;
  * @param res
  */
 module.exports = function (req, res) {
-    // let body = 'Hello World';
-    // res.writeHead(200, {
-    //     'Content-Length': Buffer.byteLength(body),
-    //     'Content-Type': 'text/plain'
-    // });
-    // res.write(body);
+    let body = 'Hello World';
+    res.writeHead(200, {
+        'Content-Length': Buffer.byteLength(body),
+        'Content-Type': 'text/plain'
+    });
+    res.write(body);
+    res.end('ok');
     // let c = maxCount;
     // while (c > 0) {
     //     crypto.createHmac('sha256', new Buffer(c))
@@ -22,7 +23,7 @@ module.exports = function (req, res) {
     //         .digest('hex');
     //     c--;
     // }
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.end('Hello World\n');
 };
