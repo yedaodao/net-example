78a1d9d9f61c45f9636c9364c7a5397ff854e4e1
yedaodao <404069912@qq.com>
2018-03-03 23:36:50 +0800

    fix http 1.0

--- http-origin/net-for-http.js
+++ http-origin/net-for-http.js
@@ -1,16 +1,22 @@
 const net = require('net');
+const fs = require('fs');
+const path = require('path');
+
+const desPort = 80;
+// const desHost = 'localhost';
+const desHost = '123.57.205.204';
 
 let allBuffer = null;
 
-const client = net.createConnection(3001, 'localhost', function() {
+const client = net.createConnection(desPort, desHost, function() {
     console.log('connected to server!');
     client.write(
-        'GET / HTTP/1.1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36\r\nAccept: text/html\r\nAccept-Language: zh-CN,zh;q=0.9\r\n\r\n'
+        'GET / HTTP/1.0\r\nHost: www.zhufengpeixun.cn\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36\r\nAccept: text/html\r\nAccept-Language: zh-CN,zh;q=0.9\r\n\r\n'
     );
 });
 
 client.on('data', function(data) {
-    console.log(data.toString());
+    console.log('------receive data------')
     if (!allBuffer) {
         allBuffer = data;
     } else {
@@ -20,9 +26,13 @@ client.on('data', function(data) {
 
 client.on('error', function(err) {
     console.log(err);
-})
+});
 
 client.on('end', function() {
     console.log('connection end');
-    console.log(allBuffer.toString());
+    const htmlContent = allBuffer.toString();
+    const ws = fs.createWriteStream(
+        path.join(__dirname, 'zhufeng.html')
+    );
+    ws.write(htmlContent);
 });
